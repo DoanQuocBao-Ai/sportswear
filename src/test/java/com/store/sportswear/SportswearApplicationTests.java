bef3d7d0c09988fff33f8a22bd5cbefe26999e5d
Bao <bao@gmail.com>
2022-11-09 00:44:02 +0700

    Initial commit

---
+++ src/test/java/com/store/sportswear/SportswearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.sportswear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportswearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
